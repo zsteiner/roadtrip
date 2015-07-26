dd51e6a08db6490e36db9e306434bf255b53fb90
Zach Steiner <zsteiner@gmail.com>
2015-07-26 14:07:10 -0400

    Build List from Mapbox Data

--- scripts/map.js
+++ scripts/map.js
@@ -1,48 +1,30 @@
 var map,
     locationList,
-    locations;
-
-function showMap(err, data) {
-  var zoomLevel = 11;
-  
-    if(address ==="") {
-        map.setView([lat, lng], zoomLevel);
-    }
-
-    else {
-        lat = data.latlng[0];
-        lng = data.latlng[1];        
-    
-        map.setView([data.latlng[0], data.latlng[1]], zoomLevel);
-    }    
-
-    marker = L.marker([lat, lng], {
-      icon: L.mapbox.marker.icon({
-        'marker-color': '#9c89cc'
-      })
-    })
-    .bindPopup('<div class="location-info-popup">' +
-    '<header class="info-popup-header"><img src="images/info-images/' + image + '.jpg">' +
-    '<div class="header-container">' + 
-    '<h1>' + locationName + '</h1>' +
-    '<h2>' + place + '</h2>' + 
-    '</div>' +
-    '</header>' +
-    '<div class="location-address">' +
-    '<div>' + address + '</div>' + 
-    '</div>' +
-    '<div class="location-note">' + note + '</div>' +
-    '</div>').addTo(map).openPopup();
-}
-
+    featureLayer;
 
 L.mapbox.accessToken = 'pk.eyJ1IjoienN0ZWluZXIiLCJhIjoiTXR4U0tyayJ9.6BxBAjPyMHbt1YfD5HWGXA';
     map = L.mapbox.map('map-canvas', 'mapbox.outdoors');
 
-var featureLayer = L.mapbox.featureLayer()
+featureLayer = L.mapbox.featureLayer()
     .loadURL('data/co-trip.geojson')
     .addTo(map);
+
+locationList = document.getElementById('location-list');
     
 featureLayer.on('ready', function() {
     map.fitBounds(featureLayer.getBounds());
+
+    featureLayer.eachLayer(function(layer) {
+              var item = locationList.appendChild(document.createElement('li'));
+              $(item).addClass('location');
+              item.innerHTML = 
+                '<div class="location-date">' + layer.toGeoJSON().properties.date + '</div>' +
+                '<div class="location-name"><a>' + layer.toGeoJSON().properties.title + '</a></div>' +
+                '<div class="location-place">' + layer.toGeoJSON().properties.place + '</div>';
+              item.onclick = function() {
+                 map.setView(layer.getLatLng(), 8);
+                 layer.openPopup();
+                 $(item).addClass('is-current').siblings().removeClass('is-current');
+              };
+    });
 });
