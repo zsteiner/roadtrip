1cd98a695420e487516b573f0bd8f9aca0e4cc9b
Zach Steiner <zsteiner@gmail.com>
2015-07-27 11:01:10 -0400

    Navigation works now

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,82 @@
 var map,
     locationList,
-    featureLayer;
+    featureLayer,
+    markers,
+    selectedMarker = 0;
+
+function navBar() {
+    
+    if (selectedMarker <= 1) {
+      $('.map-nav-control a[rel=prev]').addClass('is-hidden'); 
+      $('.map-nav-control a[rel=next]').removeClass('is-hidden'); 
+      $('.map-nav-control a[rel=begin]').addClass('is-hidden'); 
+      $('.map-nav-control a[rel=startover]').addClass('is-hidden'); 
+      $('.stop-counter').removeClass('is-hidden');
+      //console.log('The BEGINNING');
+    }
+
+    else if (selectedMarker === markers.length) {
+      $('.map-nav-control a[rel=startover]').removeClass('is-hidden'); 
+      $('.map-nav-control a[rel=prev]').removeClass('is-hidden'); 
+      $('.map-nav-control a[rel=begin]').addClass('is-hidden'); 
+      $('.map-nav-control a[rel=next]').addClass('is-hidden'); 
+      //console.log('The END');  
+    }
+    
+    else {
+        $('.map-nav-control a[rel=prev]').removeClass('is-hidden'); 
+        $('.map-nav-control a[rel=next]').removeClass('is-hidden'); 
+        $('.map-nav-control a[rel=startover]').addClass('is-hidden');  
+        $('.map-nav-control a[rel=begin]').addClass('is-hidden');
+        $('.stop-counter').removeClass('is-hidden');  
+        //console.log('The JOURNEY');
+    }
+    $('.location:nth-child(' + selectedMarker + ')').addClass('is-current').siblings().removeClass('is-current');
+    myScroll.scrollToElement(document.querySelector('.location:nth-child(' + selectedMarker + ')'), 600, null, true);
+    
+    $('#stop-number').text(selectedMarker);
+}
+
+function next() {
+    var i;
+        
+    if (selectedMarker === markers.length) {
+        i = 0;
+        map.setView(markers[i].getLatLng(), 8);
+        markers[i].openPopup();
+        //console.log("next, else if statement");       
+    }
+    
+    else {
+        i = selectedMarker - 1;
+        ++i;    
+        map.setView(markers[i].getLatLng(), 8);
+        markers[i].openPopup();
+        //console.log("next, else statement");
+    }
+    
+    selectedMarker = i + 1;
+    navBar();
+
+    //console.log("Selected = " + selectedMarker);
+    //console.log("i = " + i);
+}
+
+function prev() {
+    var i  = selectedMarker - 1;
+
+    --i;
+    map.setView(markers[i].getLatLng(), 8);
+    markers[i].openPopup();
+    console.log("prev, else statement");
+   
+    selectedMarker = i + 1;
+    navBar();
+    
+    //console.log("Selected = " + selectedMarker);
+    //console.log("i = " + i);
+}
+
 
 L.mapbox.accessToken = 'pk.eyJ1IjoienN0ZWluZXIiLCJhIjoiTXR4U0tyayJ9.6BxBAjPyMHbt1YfD5HWGXA';
     map = L.mapbox.map('map-canvas', 'mapbox.outdoors');
@@ -19,21 +95,22 @@ featureLayer.on('ready', function() {
         
         var locationDate = layer.feature.properties.date,
             locationTitle = layer.feature.properties.title,
-            //locationPlace = layer.feature.properties.place,
+            locationID = layer.feature.properties.id,
             locationImage = layer.feature.properties.image,
             locationCity = layer.feature.properties.city,
             locationStreet = layer.feature.properties.street,
             locationDescription = layer.feature.properties.description;
+            locationPlace = layer.feature.properties.place;
         
         $(item).addClass('location');
         
-        if (locationImage === '') {
+        if (locationImage === true) {
             layer.bindPopup(
-              '<header class="info-popup-header"></header>' + 
+              '<header class="info-popup-header with-image" style="background-image: url(images/background-images/background' + locationID + '.jpg);"></header>' +
               '<div class="info-popup-header-content">' + 
-                  '<div class="location-date">' + locationDate + '</div>' +
+                  '<div class="location-date"><span class="location-id">Stop #' + locationID + '</span>' + 
+                      locationDate + '</div>' +
                   '<h1 class="location-title">' + locationTitle + '</h1>' +
-                  //'<h2 class="location-place">' + locationPlace + '</h2>' +
               '</div>' +
               '<div class="location-address"><div>' + locationStreet + '</div><div>' + locationCity + '</div></div>' +
               '<div class="location-description">' + locationDescription + '</div>'
@@ -42,12 +119,11 @@ featureLayer.on('ready', function() {
         
         else {
             layer.bindPopup(
-              '<a href="images/background-images/' + locationImage + '.jpg">' +
-              '<header class="info-popup-header with-image" style="background-image: url(images/background-images/' + locationImage + '.jpg);"></header>' + '</a>' +
+              '<header class="banner-mountain info-popup-header"></header>' + 
               '<div class="info-popup-header-content">' + 
-                  '<div class="location-date">' + locationDate + '</div>' +
+                  '<div class="location-date"><span class="location-id">Stop #' + locationID + '</span>' + 
+                      locationDate + '</div>' +
                   '<h1 class="location-title">' + locationTitle + '</h1>' +
-                  //'<h2 class="location-place">' + locationPlace + '</h2>' +
               '</div>' +
               '<div class="location-address"><div>' + locationStreet + '</div><div>' + locationCity + '</div></div>' +
               '<div class="location-description">' + locationDescription + '</div>'
@@ -55,13 +131,45 @@ featureLayer.on('ready', function() {
         }
         
         item.innerHTML = 
-            '<div class="location-date">' + layer.toGeoJSON().properties.date + '</div>' +
-            '<div class="location-name"><a>' + layer.toGeoJSON().properties.title + '</a></div>' +
-            '<div class="location-place">' + layer.toGeoJSON().properties.place + '</div>';
+          '<div class="location-date"><span class="location-id">Stop #' + locationID + '</span>' + 
+              locationDate + '</div>' +
+            '<div class="location-name"><a>' + locationTitle + '</a></div>' +
+            '<div class="location-place">' + locationPlace + '</div>';
+        
         item.onclick = function() {
             map.setView(layer.getLatLng(), 8);
             layer.openPopup();
-            $(item).addClass('is-current').siblings().removeClass('is-current');
+            
+            selectedMarker = parseInt(locationID, 10);
+            navBar();
+            
+            //console.log("Selected = " + selectedMarker);
         };
+
+        layer.on('click', function() {
+            selectedMarker = parseInt(locationID, 10);
+            navBar();
+            //console.log("Selected = " + selectedMarker);
+            
+        });
+        
+    markers = [];
+        featureLayer.eachLayer(function(layer) { markers.push(layer); });
     });
+});
+
+$('.map-nav-control a[rel=begin]').click(function(){
+   next();
+});
+
+$('.map-nav-control a[rel=next]').click(function(){
+   next();
+});
+
+$('.map-nav-control a[rel=prev]').click(function(){
+   prev();
+});
+
+$('.map-nav-control a[rel=startover]').click(function(){
+   next();
 });
